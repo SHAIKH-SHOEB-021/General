1b71651f55845279bf5179a4e7a055927d65c940
vinaygupta.cont <vinaygupta.cont@niveussolutions.com>
2022-12-19 11:53:31 +0530

    Initial Commit

---
+++ MyPlayground.playground/Contents.swift
@@ -0,0 +1,24 @@
+import Foundation
+import Security
+var Word = ["hello","world","table","football","pipe","code"]
+Word.forEach { words in
+    print(words)
+}
+
+var demo = "Hello World"
+class Gender{
+    struct gender{
+        var gender = ""
+    }
+}
+
+class Person : Gender{
+    static func details(name: String, age: String){
+        print("\(name) is \(age) year old")
+    }
+}
+
+
+var obj = Person()
+Person.details(name: "Shaikh", age: "23")
+print(demo.count)
